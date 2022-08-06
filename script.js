09c6f77847ce9f11c5f3e49332f7ebd4750a8d0f
Convidium <denyssianchuk0@gmail.com>
2022-08-06 20:47:13 +0300

    first commit

--- script.js
+++ script.js
@@ -1,44 +1,105 @@
 'use strict';
 
-function getDayBudget() {
-    let money = prompt("Ваш бюджет на місяць? (грн)");
-    let time = prompt("Введіть дату в форматі YYYY/MM/DD");
+let appData = {
+    budget: 0,
+    timeData: 0,
+    expenses: {},
+    optionalExpenses: 0,
+    income: [],
+    savings: false
+};
 
-    if (typeof parseInt(money) != 'number' || money == null || isNaN(money)) {
-        alert('Данні введено неправильно! Спробуйте ще раз.');
-        getDayBudget();
-        return;
+let result;
+
+
+    function start() {
+        let money = prompt("Ваш бюджет на місяць? (грн)");
+        let time = prompt("Введіть дату в форматі YYYY/MM/DD");
+
+        while (typeof parseInt(money) != 'number' || money == null || isNaN(money)) {
+            alert('Данні введено неправильно! Спробуйте ще раз.');
+            return start();
+
+        }
+
+        appData = {
+            budget: money,
+            timeData: time,
+            expenses: {},
+            optionalExpenses: 0,
+            income: [],
+            savings: false
+        };
+
+        detectDayBudget();
     }
 
 
-    let appData = {
-        budget: money,
-        timeData: time,
-        expenses: {},
-        optionalExpenses: 0,
-        income: [],
-        savings: true
-    };
 
-    let expenseFirst = prompt("Введіть обов'язкову статтю витрат в цьому місяці (1)");
-    let expenseCostFirst = prompt("У скільки обійдеться? (грн)");
 
-    let expenseSecond = prompt("Введіть обов'язкову статтю витрат в цьому місяці (2)");
-    let expenseCostSecond = prompt("У скільки обійдеться? (грн)");
 
-    appData.expenses = {
-        expenseFirst: expenseCostFirst,
-        expenseSecond: expenseCostSecond
-    };
+    function detectDayBudget() {
+        let expenseCosts = [];
+        for (let i = 1; i <= 2; i++) {
+            let expense = prompt("Введіть обов'язкову статтю витрат в цьому місяці (" + i + ")");
 
-    let result = (parseInt(appData.expenses.expenseFirst) + parseInt(appData.expenses.expenseSecond)) / 30;
+            if (expense == null || expense == '') {
+                alert('Данні введено неправильно! Спробуйте ще раз.');
+                return start();
+            }
 
-    if (result <= appData.budget) {
-        alert('Ваш бюджет на день ' + Math.floor((parseInt(appData.expenses.expenseFirst) + parseInt(appData.expenses.expenseSecond)) / 30) + 'грн');
+            let expenseCost = prompt("У скільки обійдеться? (грн)");
+
+            if (expenseCost == null || typeof parseInt(expenseCost) != 'number' || expenseCost == '') {
+                alert('Данні введено неправильно! Спробуйте ще раз.');
+                return start();
+            }
+
+            appData.expenses[expense] = expenseCost;
+            expenseCosts.push(expenseCost);
+
+        }
+
+        result = (parseInt(expenseCosts[0]) + (parseInt(expenseCosts[1]))) / 30;
+
+
+        if (result <= appData.budget) {
+            appData.savings = true;
+            alert('Ваш бюджет на день ' + result + ' грн');
+        } else {
+            alert('Результат невірний, витрати перевищують дохід');
+        }
+
+        detectLevel();
+    }
+
+
+    function detectLevel() {
+        if (result > 18000) {
+            alert("У вас високий рівень доходів");
+        } else if (result < 18000 && result > 10000) {
+            alert("У вас середній рівень доходів");
+        } else if (result < 10000) {
+            alert("У вас низький рівень доходів");
+        }
+
+        appData.dayBudget = alert(parseInt(result) + " грн");
+
+    }
+
+
+function checkSavings() {
+    if (appData.savings == true) {
+        let save = prompt("Яка сума депозиту?");
+        let percent = prompt("Під який процент?");
+
+        appData.monthIncome = parseInt(save) / 100 * parseInt(percent);
+        alert(appData.monthIncome + " грн");
     } else {
-        alert('Результат невірний, витрати перевищують дохід');
+        alert("У вас немає доступних коштів");
     }
 }
 
-getDayBudget();
+document.getElementById('getDayBudget').onclick = start;
 
+document.getElementById('checkSavings').onclick = checkSavings;
